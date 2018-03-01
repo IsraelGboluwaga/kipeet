51d873fd050491f7b5dff8f0e3bd115c9b9a2439
Israel Arunah <israel.arunah@konga.com>
2018-03-01 16:03:34 +0100

    Partially configured app config file

--- app.js
+++ app.js
@@ -33,11 +33,27 @@ app.use(logger('dev'));
 
 //Parsing the body on each request and giving access to data sent
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Enable CORS from client-side
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST,PATCH, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
 //For routing. I think it's good practice to have the app routing somewhere else
+app.use('/health', (req, res) => {
+    return res.json({
+        status: 'Up',
+        data: 'O sha pran pran'
+    })
+});
 app.use('/', appRoutes);
 app.use('/login', login);
 app.use('/user', activity);
@@ -51,21 +67,21 @@ app.use(session({
 }));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
