f4d7a4761bbb96920dbe9d3c05562cc843ffc673
Israel Arunah <israel.arunah@konga.com>
2018-03-02 09:30:16 +0100

    Changed var declaration to const

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const constants = require('../config/constants');
 const check = require('../config/utils');
 const appName = constants.APP_NAME;
 
-let pass = {
+const templateText = {
     title: appName,
     header: appName,
     welcome_message: constants.WELCOME_MESSAGE
@@ -14,38 +14,38 @@ let pass = {
 
 /* GET home page for unregistered users */
 router.get('/', function (req, res, next) {
-    res.render('index', pass);
+    res.render('index', templateText);
 });
 
 //User's sign up
 router.post('/', (req, res, next) => {
-    let username = req.body.username;
-    let password = req.body.password;
-    let email = req.body.email;
-    let phone = req.body.phone;
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+    const phone = req.body.phone;
 
 
     if (!username && !password && !email && !phone) {
-        pass.error_message = constants.EMPTY_PARAMS;
+        templateText.error_message = constants.EMPTY_PARAMS;
         res.redirect('/')
     }
 
     if (!check.email(email)) {
-        pass.error_message = constants.INVALID_EMAIL;
+        templateText.error_message = constants.INVALID_EMAIL;
         res.redirect('/')
     }
 
     if (!check.username(username)) {
-        pass.error_message = constants.INVALID_USERNAME;
+        templateText.error_message = constants.INVALID_USERNAME;
         res.redirect('/')
     }
 
     if (!check.phone(phone)) {
-        pass.error_message = constants.INVALID_PHONE;
+        templateText.error_message = constants.INVALID_PHONE;
         res.redirect('/')
     }
 
-    let params = {
+    const params = {
         username: username,
         password: password,
         email: email,
@@ -56,7 +56,7 @@ router.post('/', (req, res, next) => {
 
     newUser.save((err, user, next) => {
         if (err) {
-            console.log(err);
+            JSON.stringify(err);
             return next(err);
         }
 
